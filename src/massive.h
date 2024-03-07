1071bf41759a67a5e92cac638de2b193b5f13d07
Darya_i <dasha2013.01.24@gmail.com>
2024-03-08 02:01:31 +0300

    add file with functions names

---
+++ src/massive.h
@@ -0,0 +1,16 @@
+//
+// created by Darya Ignatenko 7th March 2024
+//
+
+// contains the functions declarations that are used in main.c
+
+#ifndef MASSIVE_H
+#define MASSIVE_H
+
+int* create_array(int size);
+void fill_array(int* array, int size);
+int count_even(int* array, int size);
+int count_odd(int* array, int size);
+void print_array(int* array, int size);
+
+#endif
